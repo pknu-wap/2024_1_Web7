cf58696e014c270f554d8693d286e16bf114ec11
khi <llioopv@naver.com>
2024-05-06 23:26:45 +0900

    update main logic

--- src/main/java/com/dogventure/dogweb/mainLogic/controller/BasicUserController.java
+++ src/main/java/com/dogventure/dogweb/mainLogic/controller/BasicUserController.java
@@ -4,6 +4,7 @@ import com.dogventure.dogweb.dto.mainLogic.login.request.SignupRequestDto;
 import com.dogventure.dogweb.dto.mainLogic.login.response.TokenResponseDto;
 import com.dogventure.dogweb.mainLogic.service.BasicUserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/basic")
@@ -21,21 +24,21 @@ public class BasicUserController {
     private final BasicUserService service;
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponseDto> login() {
+    public ResponseEntity<TokenResponseDto> login(HttpServletResponse response) throws IOException {
 
         TokenResponseDto tokenDto = service.login();
         return new ResponseEntity<>(tokenDto, HttpStatus.OK);
     }
 
     @PostMapping("/signup")
-    public ResponseEntity signup(@RequestBody @Valid SignupRequestDto request) {
+    public ResponseEntity signup(@RequestBody @Valid SignupRequestDto request, HttpServletResponse response) throws IOException {
 
         service.signup(request);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity logout(HttpServletRequest request) {
+    public ResponseEntity logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         service.logout(request);
         return new ResponseEntity<>(HttpStatus.OK);
--- src/main/java/com/dogventure/dogweb/security/handler/CustomSuccessHandler.java
+++ src/main/java/com/dogventure/dogweb/security/handler/CustomSuccessHandler.java
@@ -8,12 +8,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.*;
 
 /*
  * 소셜 로그인을 성공적으로 수행할 경우 토큰 발행
@@ -34,7 +37,8 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         log.info("JWT 토큰이 발급되었습니다");
 
         response.addCookie(createCookie("Authorization", token));
-        response.sendRedirect("http://localhost:3000/");
+        response.setStatus(HttpStatus.OK.value());
+//        response.sendRedirect("http://localhost:3000");
     }
 
     private Cookie createCookie(String key, String token) {
